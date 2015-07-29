ebec163d081a0b016da85d18b446819c7a0d7dcd
Carlos Ruiz <c.ruiz@odey.com>
2015-07-29 14:52:31 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Security/trunk@8662 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Security.SecurityService/SecurityService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Odey.Framework.Infrastructure.Services;
+using Odey.Framework.Keeley.Entities.Enums;
+using Odey.Security.Contracts;
+
+namespace Odey.Security.SecurityService
+{
+
+    public class SecurityService : OdeyServiceBase, ISecurity
+    {
+        private readonly Security security;
+        public SecurityService()
+        {
+            security = new Security();
+        }
+
+        public Dictionary<FunctionPointIds, FunctionOperations> GetUserPermission(string adName)
+        {
+            return security.GetUserPermission(adName);
+        }
+    }
+}
