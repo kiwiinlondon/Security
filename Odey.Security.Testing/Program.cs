9ff21e7792115e53389c2c8130fd4068cd132e02
Carlos Ruiz <c.ruiz@odey.com>
2015-07-29 14:52:50 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Security/trunk@8663 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Security.Testing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Odey.Security.Clients;
+
+namespace Odey.Security.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var security = new Security();
+
+            var a = security.GetUserPermission(@"OAM\Intranet_Programmers2");
+        }
+    }
+}
