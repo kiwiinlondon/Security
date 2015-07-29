705057fc943ffa6fa42bb27326169fa8d1a41d05
Carlos Ruiz <c.ruiz@odey.com>
2015-07-29 14:51:01 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Security/trunk@8659 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Security.Clients/SecurityClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using Odey.Framework.Infrastructure.Clients;
+using Odey.Framework.Keeley.Entities.Enums;
+using Odey.Security.Contracts;
+
+namespace Odey.Security.Clients
+{
+    public class SecurityClient : OdeyClientBase<ISecurity>, ISecurity
+    {
+        public Dictionary<FunctionPointIds, FunctionOperations> GetUserPermission(string adName)
+        {
+            Dictionary<FunctionPointIds, FunctionOperations> permissions = null;
+            ISecurity proxy = factory.CreateChannel();
+            try
+            {
+                permissions = proxy.GetUserPermission(adName);
+                ((ICommunicationObject)proxy).Close();
+
+            }
+            catch
+            {
+                ((ICommunicationObject)proxy).Abort();
+                throw;
+            }
+
+            return permissions;
+        }
+    }
+}
